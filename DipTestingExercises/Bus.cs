6ae3c84ff2dfe35bd02ae891d95f931285dc7862
tim <tim@DESKTOP-GRL110G>
2017-08-23 23:13:27 +1000

    Added Files
    Added Files

---
+++ DipTestingExercises/Bus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DipTestingExercises
+{
+    public class Bus: MotorVehicle
+    {
+        public List<IPerson> passengers = new List<IPerson>();
+        public int maxPassengers { get; set; }
+
+        public Bus(IPerson pDriver, int pMaxFuel, int pCurrent, int pLitresPerKM, int pMaxPassengers):base(pDriver, pMaxFuel, pCurrent, pLitresPerKM)
+        {
+            maxPassengers = pMaxPassengers;
+        }
+
+        public void embarkPassenger(IPerson pPassenger)
+        {
+            passengers.Add(pPassenger);
+        }
+
+        public int getPassengerCount()
+        {
+            return passengers.Count;
+        }
+    }
+}
---
+++ DipTestingExercises/MotorVehicle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DipTestingExercises
+{
+    public abstract class MotorVehicle: IVehicle
+    {
+        IPerson driver;
+        public int maxFuel { get; set; }
+        public int currentFuel { get; set; }
+        public int litresPerKM { get; set; }
+
+
+        public MotorVehicle(IPerson pDriver, int pMax, int pCurrent, int pLitresPerKM)
+        {
+            driver = pDriver;
+            maxFuel = pMax;
+            currentFuel = pCurrent;
+            litresPerKM = pLitresPerKM;
+        }
+
+
+        public void refuel()
+        {
+            currentFuel = maxFuel;
+        }
+
+        public int getFuelRemaining()
+        {
+            return currentFuel;
+        }
+
+        public void travel(int pKMTavelled)
+        {
+            currentFuel -= litresPerKM * pKMTavelled;
+        }
+
+    }
+}
